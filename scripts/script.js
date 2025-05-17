07f9fdc9a1c6fee1c85f040a1e4b4fff131daabb
Vip Plug <aderitogiftvascocuco@gmail.com>
2025-05-17 13:45:36 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -23,33 +23,33 @@ function setupPaypalButton(previewNumber, price, redirectUrl) {
 // Chamadas para configurar os botões do PayPal para cada vídeo
 document.addEventListener('DOMContentLoaded', function () {
     // OMEGLE SKYPE, preço de $35 e link para o redirecionamento
-    setupPaypalButton(1, '55.00', 'https://mega.nz/folder/F1pT1SCA#9tjWbTH7Zuza46DAsWVVmg');
+    setupPaypalButton(1, '55.00', 'https://www.dropbox.com/scl/fo/ureoah8ximjpad1a04jh4/AG0aXC0YwYEdHOUTWV1XwNY?rlkey=h6enzgsw7azmcudzqjdl0cul1&e=1&dl=0');
 
     // Para o segundo vídeo, preço de $35 e link para o redirecionamento
-    setupPaypalButton(2, '35.00', 'https://mega.nz/folder/N1h0FaaJ#lvkxrdeePJR3L9rHkrQYSQ');
+    setupPaypalButton(2, '35.00', 'https://mega.nz/folder/K5pkBbwB#IrQLmok6AtLzdWTpVDJxgQ');
 
     // Para o terceiro vídeo, preço de $35 e link para o redirecionamento
-    setupPaypalButton(3, '35.00', 'https://mega.nz/folder/9oxh3aSR#IPv3b9K2uZpmYZ3KgX_HgA');
+    setupPaypalButton(3, '35.00', 'https://mega.nz/folder/uQEjlSZb#dEx7JXogoeUWT5ap4SezDw/folder/3Q90iaKB');
 
     // Para o quarto vídeo, preço de $45 e link para o redirecionamento
-    setupPaypalButton(4, '45.00', 'https://mega.nz/folder/l8Y1USLA#yAMeMCDs5bNzgLH7B-JHvg');
+    setupPaypalButton(4, '45.00', 'https://mega.nz/folder/uQEjlSZb#dEx7JXogoeUWT5ap4SezDw');
 
     // Para o quinto vídeo, preço de $30 e link para o redirecionamento
-    setupPaypalButton(5, '30.00', 'https://shre.ink/MEqZ');
+    setupPaypalButton(5, '30.00', 'https://mega.nz/folder/SjBiHZ5I#nyk27HZR_PB649wCB7uKMQ/folder/CngQ3bjC');
 
     // Para o sexto vídeo, preço de $25 e link para o redirecionamento
-    setupPaypalButton(6, '25.00', 'https://shre.ink/MEqA');
+    setupPaypalButton(6, '25.00', 'https://mega.nz/folder/QBp0mQgJ#Ads3DWmYpFANMzSQlZlNXA');
 
     // Para o sétimo vídeo, preço de $25 e link para o redirecionamento
-    setupPaypalButton(7, '45.00', 'https://shre.ink/ME56');
+    setupPaypalButton(7, '45.00', 'https://mega.nz/folder/E0QyXTrC#dkFwRfgapkWT5OOtRSGQYA');
 
     // Para o nono vídeo, preço de $25 e link para o redirecionamento
-    setupPaypalButton(9, '25.00', 'https://mega.nz/folder/4FxQUb4I#hWH91cWf0olPXMVB9LwBLw');
+    setupPaypalButton(8, '25.00', 'https://mega.nz/folder/4FxQUb4I#hWH91cWf0olPXMVB9LwBLw');
 
     // Para o décimo vídeo, preço de $25 e link para o redirecionamento
-    setupPaypalButton(10, '20.00', 'https://shre.ink/ME52');
+    setupPaypalButton(9, '20.00', 'https://mega.nz/folder/CgIAkKKK#2wegOq1VAhV1rs41elHx-w');
 
     // Para o décimo vídeo, preço de $10 e link para o redirecionamento
-    setupPaypalButton(10, '35.00', 'https://shre.ink/MELy');
+    setupPaypalButton(10, '35.00', 'https://mega.nz/folder/4oxRVD7b#yoWcK2OzdtmB0UCNaKp8nQ');
 });
 
